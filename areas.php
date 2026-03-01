e870511b55e363d9d56f67cdb2c8f7a9786f2f97
toninews <toninews@hotmail.com>
2026-03-01 20:50:53 -0300

    perf: restore synchronous css loading and remove unused area markup

--- areas.php
+++ areas.php
@@ -8,7 +8,6 @@
   <div class="row areas-row">
     <div class="col-xs-12 col-md-6">
       <div class="card">
-        <i class="icon-bank"></i>
         <h3>Direito Criminal</h3>
         <p>Atuamos com firmeza na defesa dos seus direitos em processos criminais, analisando provas, construindo estratégias jurídicas eficazes e representando você perante as autoridades competentes. Nosso compromisso é garantir justiça e proteção em cada etapa.</p>
         <a href="" class="btn area-more-btn">Saiba mais</a>
@@ -17,7 +16,6 @@
 
     <div class="col-xs-12 col-md-6">
       <div class="card">
-        <i class="icon-bank"></i>
         <h3>Direito Previdenciário</h3>
         <p>Oferecemos suporte completo em aposentadorias, pensões, auxílio-doença e demais benefícios previdenciários. Representamos seus interesses perante o INSS e órgãos competentes, garantindo que seus direitos sejam respeitados e que você receba o benefício justo.</p>
         <a href="" class="btn area-more-btn">Saiba mais</a>
@@ -26,7 +24,6 @@
 
     <div class="col-xs-12 col-md-6">
       <div class="card">
-        <i class="icon-bank"></i>
         <h3>Direito do Consumidor</h3>
         <p>Defendemos seus direitos em casos de cobranças indevidas, contratos abusivos, problemas com produtos ou serviços e outras situações de consumo. Nosso objetivo é assegurar justiça e equilíbrio nas relações entre consumidores e fornecedores.</p>
         <a href="" class="btn area-more-btn">Saiba mais</a>
@@ -35,7 +32,6 @@
 
     <div class="col-xs-12 col-md-6">
       <div class="card">
-        <i class="icon-bank"></i>
         <h3>Direito Trabalhista</h3>
         <p>Defendemos seus direitos em casos de demissões injustas, assédio, falta de pagamento de salários e demais conflitos trabalhistas. Atuamos tanto na negociação quanto em ações judiciais, sempre com foco em garantir justiça e equilíbrio nas relações de trabalho.</p>
         <a href="" class="btn area-more-btn">Saiba mais</a>
